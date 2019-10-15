0e4984f2356b43791e5289f40db0879cb4fdc1ee
werexa <werexa200@gmail.com>
2019-10-15 10:58:52 +0200

    part 1

---
+++ project_en_lessons/template/main.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <?php 
+    require_once("project_en_lessons/hooks/bootstrap_4.php");
+    ?> 
+
+</head>
+<body>
+
+<?php 
+require_once("project_en_lessons/hooks/boostrap_4_js.php");
+?>
+</body>
+</html>
+
+
+
+
